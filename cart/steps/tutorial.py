fc3d9eab1c798821e74b83fe614c810d00f9b608
Mikhail Ivanov <ivanov.mikhail@220-volt.ru>
2017-11-23 14:38:58 +0300

    Добавлен перехват исключения, возникающего при неудачном подключении к Селеноиду

--- cart/steps/tutorial.py
+++ cart/steps/tutorial.py
@@ -1,15 +1,23 @@
 #-*- coding: utf-8 -*-
+import sys
+
 from behave import *
 from selene.api import *
 from time import sleep
 from selene import browser
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
 
-driver = webdriver.Remote(
-        command_executor='http://selenoid:4444',
-        desired_capabilities={'browserName': 'chrome',
-                              'version': '62.0',
-                              'javascriptEnabled': True})
+try:
+    driver = webdriver.Remote(
+            command_executor='http://selenoid:4444/wd/hub',
+            desired_capabilities={'browserName': 'chrome',
+                                  'version': '62.0',
+                                  'javascriptEnabled': True})
+except WebDriverException, e:
+    print("Ошибка при подключении к Selenoid:", e)
+    sys.exit(1)
+
 browser.set_driver(driver)
 
 @Given('open http//:220-volt.ru')
