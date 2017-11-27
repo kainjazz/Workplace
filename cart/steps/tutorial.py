2c09e3d0e1b77925c577bc3e0aa63bcf08f4bd80
Mikhail Ivanov <ivanov.mikhail@220-volt.ru>
2017-11-27 14:08:02 +0300

    Общий код (подключение к селеноиду) вынесен в отдельный файл

--- cart/steps/tutorial.py
+++ cart/steps/tutorial.py
@@ -8,22 +8,10 @@ from selene import browser
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 
-try:
-    driver = webdriver.Remote(
-            command_executor='http://selenoid:4444/wd/hub',
-            desired_capabilities={'browserName': 'chrome',
-                                  'version': '62.0',
-                                  'javascriptEnabled': True})
-except WebDriverException, e:
-    print("Ошибка при подключении к Selenoid:", e)
-    sys.exit(1)
-
-browser.set_driver(driver)
-
-@Given('open http//:220-volt.ru')
-def step_impl(context):
+@Given("open '{url}'")
+def step_impl(context, url):
     # config.browser_name = BrowserName.CHROME
-    browser.open_url('http://www.220-volt.ru/')
+    browser.open_url(url)
 
 @when('press "login"')
 def step_impl(context):
